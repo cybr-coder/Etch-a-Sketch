addd1d1e65e2fa901ef9f4abe26208934b0d0616
cybr-coder <codercybr@gmail.com>
2022-11-02 15:16:59 -0400

    Added RGBA colors

--- index.js
+++ index.js
@@ -1,22 +1,19 @@
 const container = document.getElementById("container");
 const button = document.getElementById("prompt-btn")
 
+
 function makeGrid(gridSize) {
   container.style.setProperty('grid-template-rows', `repeat(${gridSize}, ${(1 / gridSize) * 600}px)`);
   container.style.setProperty('grid-template-columns', `repeat(${gridSize}, ${(1 / gridSize) * 650}px)`);
-  
-  
+
   for (i = 0; i < (gridSize * gridSize); i++) {
     let squares = document.createElement("div");
     container.appendChild(squares).className = "grid-item";  
+    const randomColor = "#" + Math.floor(Math.random() * (256 * 256 * 256)).toString(16).padStart(6, "0");
     squares.addEventListener("mouseover", () => {
-    squares.style.backgroundColor = "black";
+    squares.style.backgroundColor = randomColor;
     })
   };
-  
-  
-
-
 };
 makeGrid(16)
 
