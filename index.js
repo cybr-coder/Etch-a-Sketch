521b32f75629406b206b6769effd488f5c09242e
cybr-coder <codercybr@gmail.com>
2022-11-02 12:08:57 -0400

    Fixed Layout

--- index.js
+++ index.js
@@ -1,37 +1,34 @@
 const container = document.getElementById("container");
 const button = document.getElementById("prompt-btn")
 
-function makeRows(rows, cols) {
-  container.style.setProperty('--grid-rows', rows);
-  container.style.setProperty('--grid-cols', cols);
-  container.style.width = "1080px";
-  for (i = 0; i < (rows * cols); i++) {
+function makeGrid(gridSize) {
+  container.style.setProperty('grid-template-rows', `repeat(${gridSize}, ${(1 / gridSize) * 600}px)`);
+  container.style.setProperty('grid-template-columns', `repeat(${gridSize}, ${(1 / gridSize) * 650}px)`);
+  
+  
+  for (i = 0; i < (gridSize * gridSize); i++) {
     let squares = document.createElement("div");
-    container.appendChild(squares).className = "grid-item";
-    squares.style.minHeight = "0";
-    squares.style.minHeight = "0";
-    squares.style.overflow = "hidden";    
+    container.appendChild(squares).className = "grid-item";  
     squares.addEventListener("mouseover", () => {
     squares.style.backgroundColor = "black";
     })
   };
-  button.addEventListener("click", () => {
-    document.querySelectorAll(".grid-item").forEach(e => e.remove())
-    let userInput = prompt("Set Number Of Squares")
-    if(userInput > 100) {
-        alert("You must enter a number below 100!");
-        return;
-    }
-    rows = userInput;
-    cols = userInput;
-    makeRows(rows, cols)
-    // if (userInput != (/^[0-9.,]+$/)){
-    //     alert("Enter a number")
-    // }
-  })
+  
+  
 
 
 };
-makeRows(10, 10)
+makeGrid(16)
 
 
+button.addEventListener("click", () => {
+  document.querySelectorAll(".grid-item").forEach(e => e.remove())
+  let userInput = prompt("Set Number Of Squares")
+  if(userInput > 100) {
+      alert("You must enter a number below 100!");
+      makeGrid(16)
+  }
+  gridSize = userInput;
+  makeGrid(gridSize)
+
+})
